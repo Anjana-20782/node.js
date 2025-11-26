7bc67dbf7869978a18f3e9575bbf137b5ad8643c
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-26 13:08:19 +0530

    new contents added in nodejs

--- fs.js
+++ fs.js
@@ -1,24 +1,38 @@
-var fs= require("fs")
+var fs= require("fs/promises")
 
 
 //Read file(asyn)
 
-fs.readFile("./text.txt","utf-8",function(err,data){
-    if(err)
-    {
-        console.log(err);
+// fs.readFile("./text.txt","utf-8",function(err,data){
+//     if(err)
+//     {
+//         console.log(err);
         
-    }
-    console.log(data);
+//     }
+//     console.log(data);
     
-})
+// })
 
 
 //Read file (sync)
 
-try {
-  const data = fs.readFileSync('./text.txt', 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error('Read error:', err);
+// try {
+//   const data = fs.readFileSync('./text.txt', 'utf8');
+//   console.log(data);
+// } catch (err) {
+//   console.error('Read error:', err);
+// }
+
+
+//Read file (promise)
+
+async function readFileAsync() {
+  try {
+    const data = await fs.readFile('./text.txt', 'utf8');
+    console.log(data);
+  } catch (err) {
+    console.error('readFile promise error:', err);
+  }
 }
+
+readFileAsync()
