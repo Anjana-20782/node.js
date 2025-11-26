97a72eebf1e2563854557ee86923ec3d8338418d
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-26 13:05:50 +0530

    new contents added in nodejs

--- fs.js
+++ fs.js
@@ -1,7 +1,7 @@
 var fs= require("fs")
 
 
-//Read file
+//Read file(asyn)
 
 fs.readFile("./text.txt","utf-8",function(err,data){
     if(err)
@@ -12,3 +12,13 @@ fs.readFile("./text.txt","utf-8",function(err,data){
     console.log(data);
     
 })
+
+
+//Read file (sync)
+
+try {
+  const data = fs.readFileSync('./text.txt', 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error('Read error:', err);
+}
