d3560a0eb1f5a81601174e37e8725058642e2831
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-26 14:38:34 +0530

    new contents added in nodejs

--- delete.js
+++ delete.js
@@ -11,3 +11,30 @@ fs.unlink("./text.txt",(err)=>{
     console.log("File Deleted");
     
 });
+
+
+//sync
+
+try {
+  fs.unlinkSync('./text.txt');
+  console.log('File deleted (sync)');
+} catch (err) {
+  console.error('unlinkSync error:', err);
+}
+
+
+//promises
+
+async function deleteFile() {
+  try {
+    await fs.unlink('./text.txt');
+    console.log('File deleted (promise)');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.error('File not found');
+    } else {
+      console.error('Unlink error:', err);
+    }
+  }
+}
+deleteFile();
--- rename.js
+++ rename.js
@@ -10,3 +10,27 @@ if(err)
 }
 console.log("File created");
 });
+
+
+
+//sync
+
+try {
+  fs.renameSync('./new.txt', './sample.txt');
+  console.log('Renamed (sync)');
+} catch (err) {
+  console.error('renameSync error:', err);
+}
+
+//promises
+async function renamePromise(src, dest) {
+  try {
+    await fsp.rename(src, dest);
+    console.log('Renamed (promise)');
+  } catch (err) {
+    console.error('Rename (promise) failed:', err);
+  }
+}
+
+renamePromise('./new.txt', './sample.txt');
+
