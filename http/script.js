535dbd310915c0dff29aed48d6ea01ed822f76b4
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-27 13:00:56 +0530

    new contents added in node

--- http/script.js
+++ http/script.js
@@ -1,11 +1,17 @@
-    const http = require('http');
+    // const http = require('http');
 
-    const server = http.createServer((req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello, World!\n');
-    });
+    // const server = http.createServer((req, res) => {
+    //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+    //   res.write("<body>")
+    //   res.end('Hello, World!\n');
+    // });
 
-    const PORT = 3000;
-    server.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}/`);
-    });
+    // const PORT = 3000;
+    // server.listen(PORT, () => {
+    //   console.log(`Server running at http://localhost:${PORT}/`);
+    // });
+
+
+const http = require('http');
+
+
