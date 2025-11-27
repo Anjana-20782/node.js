ffc9ad436d8313e159e430b66d9b1e0db4c0cf49
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-27 13:13:49 +0530

    new contents added in node

--- http/script.js
+++ http/script.js
@@ -12,14 +12,17 @@
     // });
 
 
-const http = require('http');
+// const http = require('http');
 
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
 
-  res.write('Part 1\n');   // send chunk 1
-  res.write('Part 2\n');   // send chunk 2
+//   res.write('Part 1\n');   // send chunk 1
+//   res.write('Part 2\n');   // send chunk 2
 
-  res.end('Final part\n'); // send final chunk and close
-}).listen(3001);
+//   res.end('Final part\n'); // send final chunk and close
+// }).listen(3001);
+
+
+
