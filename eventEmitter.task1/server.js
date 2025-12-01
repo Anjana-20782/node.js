3beed85165284b981336f431ab329f3ee5c51808
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-01 16:08:12 +0530

    new contents added in node js

---
+++ eventEmitter.task1/server.js
@@ -0,0 +1,27 @@
+// countdown.js
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+function startCountdown() {
+  const numbers = [3, 2, 1];
+
+  // run each count with a 1-second gap
+  numbers.forEach((num, i) => {
+    setTimeout(() => {
+      console.log(num);
+      // when last number printed, emit the event after a tiny delay
+      if (i === numbers.length - 1) {
+        // small delay to show separation, optional
+        setTimeout(() => emitter.emit('launch:ready'), 200);
+      }
+    }, i * 1000); // 0ms, 1000ms, 2000ms
+  });
+}
+
+// listener
+emitter.on('launch:ready', () => {
+  console.log('Launch protocol approved');
+});
+
+// start
+startCountdown();
