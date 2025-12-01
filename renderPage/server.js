7a411bf8e977b8a109b447fb0aa5ed2b211b9f30
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-01 15:11:16 +0530

    new contents added in node js

---
+++ renderPage/server.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  const { url, method } = req;
+
+
+  if (url.endsWith(".css") || url.endsWith(".js")) {
+    const assetPath = path.join(__dirname, "public", url);
+    const ext = path.extname(url);
+
+    const contentType =
+      ext === ".css" ? "text/css" : "application/javascript";
+
+    fs.readFile(assetPath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        return res.end("Asset not found.");
+      }
+
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    });
+
+    return;
+  }
+
+  
+  if (url === "/" && method === "GET") {
+    return renderPage(res, "home.html");
+  }
+
+  if (url === "/login" && method === "GET") {
+    return renderPage(res, "login.html");
+  }
+
+  if (url === "/sign" && method === "GET") {
+    return renderPage(res, "sign.html");
+  }
+
+  
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Route not found");
+
+ 
+  function renderPage(res, page) {
+    const pagePath = path.join(__dirname, "views", page);
+
+    fs.readFile(pagePath, (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        return res.end("Failed to load page");
+      }
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  }
+});
+
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
