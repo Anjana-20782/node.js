12f0022b3cf933b0d08acea93d5a68b50fc43dbb
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-12-01 17:11:30 +0530

    new contents added in node js

---
+++ eventEmitter.task1/notificationbus.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+const notificationBus = new EventEmitter();//instance cheya
+
+notificationBus.on("newNotification", (data) => {
+  console.log("New notification received:", data.message);
+
+  console.log("Sender:", data.sender);
+  console.log("Time:", data.time);
+});
+
+notificationBus.emit("newNotification", {
+  message: "Your order has been successfully placed!",
+  sender: "Order Service",
+  time: new Date().toLocaleString()
+});
